fb6fbc2ffa6395d7eeaa7b367b1d7d11ef812f59
Jurabek <jurabekdeveloper@gmail.com>
2022-02-17 17:10:19 +0500

    added profile page

--- lib/pages/restaurant_owner_screens/owner_setting_screen.dart
+++ lib/pages/restaurant_owner_screens/owner_setting_screen.dart
@@ -1,4 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:inadvance/pages/restaurant_owner_screens/restaurant_profile_page.dart';
+import 'package:inadvance/utils/colors.dart';
+import 'dart:io' show Platform;
+
+import '../about_app_page.dart';
+import '../choose_language_page.dart';
 
 class OwnerSettingScreen extends StatefulWidget {
   const OwnerSettingScreen({Key? key}) : super(key: key);
@@ -8,12 +15,176 @@ class OwnerSettingScreen extends StatefulWidget {
 }
 
 class _OwnerSettingScreenState extends State<OwnerSettingScreen> {
+
+  void _iosDialog(){
+    showDialog(context: context,
+        builder: (BuildContext context) {
+          return CupertinoAlertDialog(
+            title: Text('Logout'),
+            content: Text('Are you sure you want to logout?'),
+            actions: [
+              CupertinoDialogAction(
+                  isDefaultAction: true,
+                  onPressed: () {},
+                  child: Text('Cancel',style: TextStyle(color: MainColors.greenColor))),
+              CupertinoDialogAction(
+                  isDefaultAction: true,
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Confirm',style: TextStyle(color: Colors.redAccent),)),
+            ],
+          );
+        }
+    );
+  }
+  void _androidDialog(){
+    showDialog(context: context,
+        builder: (BuildContext context){
+          return AlertDialog(
+            title: Text('Logout'),
+            content: Text('Are you sure you want to Logout!'),
+            actions: [
+              TextButton(
+                child: Text('Cancel'),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+              ),
+              TextButton(
+                child: Text('Confirm'),
+                onPressed: (){
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        }
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade50,
       appBar: AppBar(
-        title: Text("Setting"),
+        title: Text("Sozlamalar"),
       ),
+      body: Column(
+        children: [
+          const Spacer(
+            flex: 5,
+          ),
+          Center(
+            child: Column(
+              children: [
+                Container(
+                  height: 115,
+                  width: 115,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            "https://scontent.ftas2-2.fna.fbcdn.net/v/t1.6435-1/147758135_2883057468590967_3283438224948433812_n.jpg?stp=dst-jpg_p720x720&_nc_cat=105&ccb=1-5&_nc_sid=1eb0c7&_nc_ohc=GzSj92nGIEAAX-EF1pd&_nc_ht=scontent.ftas2-2.fna&oh=00_AT_v--ZR3tfNgI1omykU60HmU2ULGmGL9NRF7z9fFT6jXQ&oe=6232FF9A"),
+                        fit: BoxFit.fill),
+                    color: Colors.grey,
+                    shape: BoxShape.circle,
+                    border:
+                    Border.all(width: 2.5, color: MainColors.greenColor),
+                  ),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                        begin: Alignment.center,
+                        colors: [
+                          Colors.black.withOpacity(.5),
+                          Colors.black.withOpacity(.5),
+                          Colors.black.withOpacity(.5),
+                        ],
+                      ),
+                      shape: BoxShape.circle,
+                    ),
+                    child: Center(
+                      child: Text(
+                        "Rayhon"[0],
+                        style: TextStyle(
+                            color: MainColors.whiteColor, fontSize: 40),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Rayhon",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                ),
+              ],
+            ),
+          ),
+          const Spacer(
+            flex: 15,
+          ),
+          InkWell(
+            child: settingInfos(nameInfo: "Restaurant profili"),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => RestProfilePage()));
+            },
+          ),
+          settingInfos(nameInfo: "To'lov tarixi"),
+          InkWell(
+            child: settingInfos(nameInfo: "Ilova haqida"),
+            onTap: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                  builder: (BuildContext context) => AboutAppPage()));
+            },
+          ),
+          settingInfos(nameInfo: "Contact"),
+          InkWell(child: settingInfos(nameInfo: "Languages"), onTap: (){
+            Navigator.of(context).push(MaterialPageRoute(builder: (BuildContext context) => ChooseLanguage()));
+          },),
+          InkWell(onTap: () {
+            if(Platform.isAndroid){
+              _androidDialog();
+            }else if(Platform.isIOS){
+              _iosDialog();
+            }
+          }, child: settingInfos(nameInfo: "Log Out")),
+          Spacer(
+            flex: 50,
+          ),
+        ],
+      ),
+    );
+  }
+  Widget settingInfos({required String nameInfo}) {
+    return Column(
+      children: [
+        Divider(
+          thickness: 0.5,
+          height: 0,
+          color: Colors.grey,
+        ),
+        ListTile(
+          title: Text(
+            nameInfo,
+            style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.bold,
+                color: nameInfo == "Log Out"
+                    ? Colors.redAccent
+                    : MainColors.blackColor),
+          ),
+          trailing: Icon(
+            Icons.navigate_next,
+            color: nameInfo == "Log Out"
+                ? Colors.redAccent
+                : MainColors.blackColor,
+            size: 30,
+          ),
+          tileColor: MainColors.whiteColor,
+        ),
+      ],
     );
   }
 }
---
+++ lib/pages/restaurant_owner_screens/restaurant_profile_page.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:inadvance/utils/colors.dart';
+import 'package:inadvance/utils/responsive_size.dart';
+
+class RestProfilePage extends StatefulWidget {
+  const RestProfilePage({Key? key}) : super(key: key);
+
+  @override
+  _RestProfilePageState createState() => _RestProfilePageState();
+}
+
+class _RestProfilePageState extends State<RestProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: SvgPicture.asset(
+              "assets/images/vector_ok.svg",
+              height: 22.5,
+            ),
+          ),
+          // SizedBox(
+          //   width: 10,
+          // ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                height: 200,
+                margin: EdgeInsets.only(bottom: 50),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(8),
+                    image: DecorationImage(
+                        fit: BoxFit.fill,
+                        image: NetworkImage(
+                            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Barbieri_-_ViaSophia25668.jpg/1200px-Barbieri_-_ViaSophia25668.jpg"))),
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                      Colors.black.withOpacity(.4),
+                      Colors.black.withOpacity(.4),
+                      Colors.black.withOpacity(.4),
+                    ]),
+                  ),
+                ),
+              ),
+              Positioned(
+                left: SizeConfig.screenWidth! / 2.5,
+                top: 150,
+                child: Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: MainColors.dimRedColor,
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              "https://scontent.ftas2-2.fna.fbcdn.net/v/t1.6435-1/147758135_2883057468590967_3283438224948433812_n.jpg?stp=dst-jpg_p720x720&_nc_cat=105&ccb=1-5&_nc_sid=1eb0c7&_nc_ohc=GzSj92nGIEAAX-EF1pd&_nc_ht=scontent.ftas2-2.fna&oh=00_AT_v--ZR3tfNgI1omykU60HmU2ULGmGL9NRF7z9fFT6jXQ&oe=6232FF9A")),
+                      border:
+                          Border.all(width: 3, color: MainColors.greenColor)),
+                ),
+              ),
+              Positioned(
+                top: 150,
+                left: SizeConfig.screenWidth! / 1.15,
+                child: Container(
+                  height: 45,
+                  width: 45,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(colors: [
+                      Colors.white.withOpacity(.3),
+                      Colors.white.withOpacity(.3),
+                      Colors.white.withOpacity(.3)
+                    ]),
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.camera_alt_outlined,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 150,
+                left: SizeConfig.screenWidth! / 1.8,
+                child: Container(
+                  height: 40,
+                  width: 40,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: MainColors.whiteColor,
+                      border: Border.all(
+                        width: 3,
+                        color: MainColors.greenColor,
+                      )),
+                  child: Center(
+                    child: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.camera_alt_outlined,
+                        color: MainColors.greenColor,
+                        size: 20,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          textfield(labelText: "Restoran nomi"),
+          textfield(labelText: "Admin raqami"),
+          textfield(labelText: "Shahar va lokatsiya", icon: Icons.location_on),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              timeInput(isOpen: "Ochiladi", time: "6:00"),
+              timeInput(isOpen: "Yopiladi", time: "21:00"),
+            ],
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          textfield(labelText: "Restoran hisob raqami")
+        ],
+      ),
+    );
+  }
+
+  Widget textfield({required String labelText, IconData? icon}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+      child: TextFormField(
+          decoration: InputDecoration(
+              labelText: labelText,
+              suffixIcon: Icon(
+                icon,
+                color: MainColors.greenColor,
+              ),
+              // labelStyle: TextStyle(color: MainColors.greenColor),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                    color: MainColors.greenColor,
+                    width: 1,
+                    style: BorderStyle.solid),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                    color: MainColors.textFieldColor,
+                    width: 1,
+                    style: BorderStyle.solid),
+              ))),
+    );
+  }
+
+  Widget timeInput({required String isOpen, required String time}) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          isOpen,
+          style: TextStyle(fontSize: 17),
+        ),
+        Container(
+          height: 40,
+          margin: EdgeInsets.only(top: 5),
+          width: SizeConfig.screenWidth! / 2.3,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            color: MainColors.greenColor.withOpacity(.3),
+          ),
+          child: TextField(
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: time,
+                contentPadding: EdgeInsets.only(left: 20, top: 5),
+                suffixIcon: InkWell(
+                    onTap: () {},
+                    child: Icon(
+                      Icons.navigate_next_sharp,
+                      color: Colors.black,
+                    ))),
+          ),
+        )
+      ],
+    );
+  }
+}
